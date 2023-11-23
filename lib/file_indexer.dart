b6810bbef9365e009eec8b104cd423140a505340
Juriba <Juribaalsiari@gmail.com>
2023-11-23 20:13:19 +0300

    let's have fun

---
+++ lib/file_indexer.dart
@@ -0,0 +1,10 @@
+/// file_indexer, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate file_indexer
+/// dart pub global activate file_indexer
+///
+/// # see usage
+/// file_indexer --help
+/// ```
+library file_indexer;
